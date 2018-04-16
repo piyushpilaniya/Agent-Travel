4cfab792798ab7bbaeddd0f9cf5dea69e3338657
Prinshu Kumar <2016csb1051@iitrpr.ac.in>
2018-04-16 13:37:27 +0530

    updated index.php

--- Nearby Sites/index.php
+++ Nearby Sites/index.php
@@ -17,6 +17,32 @@
         }
       echo $city;
 	 
+
+function curl($url) {
+        
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+            $data = curl_exec($ch);
+            curl_close($ch);
+
+            return $data;
+        } 
+
+         $urlContents = curl("http://api.openweathermap.org/data/2.5/weather?q=".$city."&type=accurate&appid=cd3f619625a99d256fcf8c12bf3cbb12");
+        
+        $weatherArray = json_decode($urlContents, true);
+        
+        $weather = "The weather in ".$city." is currently ".$weatherArray['weather'][0]['description'].".";
+        
+        $tempInFahrenheit = intval($weatherArray['main']['temp']* 9/5 - 459.67);
+        
+        $speedInMPH = intval($weatherArray['wind']['speed']*2.24);
+        
+        $weather .=" The temperature is ".$tempInFahrenheit."&deg; F with a wind speed of ".$speedInMPH." MPH.";
+        echo $weather;
+
 	 ?>
 	
 	 <h1>Enter radius around which you want to see the result</h1>
@@ -37,6 +63,8 @@
 	 <?php
 	 	}
 	 ?>
+
+
 
 </body>
 </html>
