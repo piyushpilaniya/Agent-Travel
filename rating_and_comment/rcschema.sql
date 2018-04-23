7b9c0245b27ab56358a2e982c1a2382c2febd4a9
Piyush Pilaniya <piyushpilaniya98@gmail.com>
2018-04-23 13:48:03 +0530

    Create rcschema.sql

---
+++ rating_and_comment/rcschema.sql
@@ -0,0 +1,17 @@
+DROP SCHEMA IF EXISTS rc;
+CREATE SCHEMA rc;
+USE rc;
+
+CREATE TABLE rating (
+ `rating_id` int(11) NOT NULL AUTO_INCREMENT,
+ `login_id` int(11),
+ `hotel_id` int(11),
+ `transport_id` int(11),
+ `rating_number` int(11),
+ `total_points` int(11),
+ `rated_time` datetime,
+ `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 = Block, 0 = Unblock',
+ PRIMARY KEY (`rating_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
