cdbc7d3b322cc8f85b52ba4459cea16c07ae1dc5
Piyush Pilaniya <piyushpilaniya98@gmail.com>
2018-04-23 14:44:36 +0530

    Update rcschema.sql

--- rating_and_comment/rcschema.sql
+++ rating_and_comment/rcschema.sql
@@ -2,10 +2,20 @@ DROP SCHEMA IF EXISTS rc;
 CREATE SCHEMA rc;
 USE rc;
 
-CREATE TABLE rating (
+CREATE TABLE hotel_rating (
  `rating_id` int(11) NOT NULL AUTO_INCREMENT,
  `login_id` int(11),
  `hotel_id` int(11),
+ `rating_number` int(11),
+ `total_points` int(11),
+ `rated_time` datetime,
+ `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 = Block, 0 = Unblock',
+ PRIMARY KEY (`rating_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE transportation_rating (
+ `rating_id` int(11) NOT NULL AUTO_INCREMENT,
+ `login_id` int(11),
  `transport_id` int(11),
  `rating_number` int(11),
  `total_points` int(11),
@@ -14,4 +24,25 @@ CREATE TABLE rating (
  PRIMARY KEY (`rating_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+CREATE TABLE transportation_comments (
+ `rating_id` int(11) NOT NULL AUTO_INCREMENT,
+ `login_id` int(11),
+ `transport_id` int(11),
+ `rating_number` int(11),
+ `total_points` int(11),
+ `rated_time` datetime,
+ `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 = Block, 0 = Unblock',
+ PRIMARY KEY (`rating_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE hotel_comments (
+ `rating_id` int(11) NOT NULL AUTO_INCREMENT,
+ `login_id` int(11),
+ `hotel_id` int(11),
+ `rating_number` int(11),
+ `total_points` int(11),
+ `rated_time` datetime,
+ `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 = Block, 0 = Unblock',
+ PRIMARY KEY (`rating_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
